b832e037e267d98c7aed657e2951a73bce20b8e9
yusuf-analyst <yusufxertex@gmail.com>
2026-01-05 14:25:01 +0000

    Create practice.py

---
+++ GCSE-CS/GCSE-CS/practice.py
@@ -0,0 +1,16 @@
+print("Practising GCSE Computer Science for data analytics")
+GCSE Computer Science practice notes
+
+Topics I understand:
+- Algorithms
+- Flowcharts
+- Pseudocode
+- Data types
+- Variables
+
+Goal: apply this knowledge to data analytics.
+print("9+1")
+a=15
+b=17
+x=a+b
+print("x")
