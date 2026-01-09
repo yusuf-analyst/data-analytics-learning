a0d2f3708babb0c865f44b5241bd58c38bf11fee
yusuf-analyst <yusufxertex@gmail.com>
2026-01-09 19:18:17 +0000

    Update practice.py

--- GCSE-CS/GCSE-CS/practice.py
+++ GCSE-CS/GCSE-CS/practice.py
@@ -8,9 +8,25 @@ Topics I understand:
 - Data types
 - Variables
 
-Goal: apply this knowledge to data analytics.
+#Goal: apply this knowledge to data analytics.
 print("9+1")
 a=15
 b=17
 x=a+b
 print("x")
+
+number=input("Enter a binary number")
+isCorrect=True
+for digit in number:
+  if digit !=("0") and digit !=("1"):
+    isCorrect=False
+
+if isCorrect==True:
+  print("Thats a binary number")
+else:
+  print("thats not a binary number")
+  #Data type switching
+  round1=int(input("Round 1: "))
+  round2=int(input("Round 2: "))
+  totalScore=round1+round2
+  print(totalScore)
